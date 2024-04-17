34fb817feb560f03ef323658129874b1bdc5cc96
lelz99 <emanuelesimonetti99@gmail.com>
2024-04-17 17:40:08 +0200

    refactoring users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,8 +13,10 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
             $table->string('email')->unique();
+            $table->string('name', 30)->nullable();
+            $table->string('surname', 50)->nullable();
+            $table->date('birth_date')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
