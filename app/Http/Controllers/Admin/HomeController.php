2351c24c25473736f5f19ef3247f54935691409e
FrancescoM95 <mon95fran@gmail.com>
2024-04-18 15:45:15 +0200

    admin home/index route

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -3,12 +3,13 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Apartment;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function __invoke()
     {
-        return view('home');
+        return view('admin.home');
     }
 }
