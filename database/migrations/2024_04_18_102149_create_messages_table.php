d103966c8c6806adbd75aadc8a7e6ef755639f66
AlessiaOsman <alessia.osman@gmail.com>
2024-04-18 12:37:24 +0200

    created message msfc

---
+++ database/migrations/2024_04_18_102149_create_messages_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('messages', function (Blueprint $table) {
+            $table->id();
+            $table->string('email');
+            $table->text('text');
+            $table->varchar('name', 30)->nullable();
+            $table->varchar('surname', 30)->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('messages');
+    }
+};
