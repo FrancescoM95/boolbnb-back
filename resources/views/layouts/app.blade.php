fb4683d2c48e49efa51b9a541129d88c2eea1ecd
AndreaScaranari <andrea.scaranari.93@gmail.com>
2024-04-19 11:45:37 +0200

    reset app blade

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,9 +10,6 @@
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
-    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css'
-        integrity='sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A=='
-        crossorigin='anonymous' />
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
@@ -31,9 +28,6 @@
         </main>
     </div>
 
-    @include('includes.modal')
-
-    @yield('scripts')
 </body>
 
 </html>
