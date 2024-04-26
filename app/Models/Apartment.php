82f7630ff78fbbe638eedb9eafabd4e29033f16b
AlessiaOsman <alessia.osman@gmail.com>
2024-04-26 09:43:26 +0200

    added image function

--- app/Models/Apartment.php
+++ app/Models/Apartment.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
@@ -46,5 +47,10 @@ class Apartment extends Model
     public function printImage()
     {
         return asset('storage/' . $this->cover_image);
+    }
+
+    public function image() :Attribute 
+    {
+        return Attribute::make(fn ($value) => url('storage/' .$value));
     }
 }
