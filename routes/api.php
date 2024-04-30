a2586db68749a18d99ec0913619a9a30c6528634
AlessiaOsman <alessia.osman@gmail.com>
2024-04-30 15:54:40 +0200

    added Service Controller and route

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\ApartmentController;
+use App\Http\Controllers\Api\ServiceController;
 use App\Models\Apartment;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -22,5 +23,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 //* Rotta per ricerca con filtro per distanza
 Route::get('apartments/search', [ApartmentController::class, 'search'])->name('apartments.search');
+
+Route::get('services', [ServiceController::class, 'index']);
 
 Route::apiResource('apartments', ApartmentController::class);
