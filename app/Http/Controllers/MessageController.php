856caa4d6391befc9f65e4ccc3c556f85f2aae87
AlessiaOsman <alessia.osman@gmail.com>
2024-05-02 20:15:24 +0200

    added validations

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -9,17 +9,27 @@ class MessageController extends Controller
 {
     public function store(Request $request)
     {
-        $data = $request->validate([
-            'name' => 'nullable',
-            'surname' => 'nullable',
-            'email' => 'required|email',
-            'text' => 'required',
-            'apartment_id' => 'nullable'
-        ]);
+        $data = $request->validate(
+            [
+                'name' => 'nullable',
+                'surname' => 'nullable',
+                'email' => 'required|email',
+                'text' => 'required | string',
+                'apartment_id' => 'nullable'
+            ],
+            [
+                'email.required' => 'La mail è obbligatoria',
+                'email.email' => 'La mail non è valida',
+                'text.required' => 'Il messaggio è obbligatorio',
+            ]
+        );
+
+        
 
         $message = new Message();
         $message->fill($data);
         $message->save();
+
 
         return response()->json(['message' => 'Message stored successfully']);
     }
