bcf8fc5584a275f7a6a24c0364eddd3e2be8135a
FrancescoM95 <mon95fran@gmail.com>
2024-05-03 09:35:27 +0200

    added reply button to messages

--- resources/views/admin/messages/index.blade.php
+++ resources/views/admin/messages/index.blade.php
@@ -15,23 +15,23 @@
             <table class="table table-striped table-hover my-3">
                 <thead>
                     <tr>
+                        <th scope="col" class="col-auto">Data</th>
                         <th scope="col" class="col-2">Nome e Cognome</th>
                         <th scope="col" class="col-2">Email</th>
-                        <th scope="col" class="col-3">Messaggio</th>
-                        <th scope="col" class="col-2">Data</th>
+                        <th scope="col" class="col-4">Messaggio</th>
                         <th scope="col" class="col-1">Visualizzato</th>
-                        <th scope="col" class="col-2"></th>
+                        <th scope="col" class="col-auto"></th>
                     </tr>
                 </thead>
                 <tbody>
                     {{-- per ogni messaggio riempi riga --}}
                     @forelse ($messages as $message)
                         <tr>
+                            <td>{{ $message->created_at->format('d/m/Y H:i') }}</td>
                             <td>{{ $message->name }} {{ $message->surname }}</td>
                             <td>{{ $message->email }}</td>
                             <td>{{ $message->getAbstract($message->text) }}{{ strlen($message->text) > 20 ? ' [...]' : '' }}
                             </td>
-                            <td>{{ $message->created_at->format('d/m/Y H:i') }}</td>
                             <td>
                                 <form action="{{ route('admin.messages.read', $message->id) }}" method="POST">
                                     @csrf
@@ -43,6 +43,8 @@
                             <td class="text-center d-flex justify-content-center gap-2">
                                 <a href="{{ route('admin.messages.show', [$apartment->id, $message->id]) }}"
                                     class="btn btn-sm btn-primary" title="Dettagli"><i class="far fa-eye"></i></a>
+                                <a href="mailto:{{ $message->email }}" class="btn btn-sm btn-success"
+                                    title="Rispondi via email"><i class="fas fa-reply"></i></a>
                                 <form action="{{ route('admin.messages.destroy', [$apartment->id, $message->id]) }}"
                                     method="POST">
                                     @csrf
