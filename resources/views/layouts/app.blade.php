506e9ff485ddd624859d77ccedac5f92b31bad53
FrancescoM95 <mon95fran@gmail.com>
2024-05-03 11:09:41 +0200

    icon fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,7 +9,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ env('APP_NAME', 'Boolbnb') }}</title>
-    <link rel="icon" href="{{ asset('/storage/Boolbnb-icon.png') }}">
+    <link rel="icon" href="{{ Vite::asset('resources/img/Boolbnb-icon.png') }}">
 
     <!--§ font-awesome -->
     <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css'
