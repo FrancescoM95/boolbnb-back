aea02b79c8309fedabca3cab5c796ba3a5e2b41d
AlessiaOsman <alessia.osman@gmail.com>
2024-05-08 18:06:47 +0200

    fixed

--- resources/views/admin/apartments/payment-confirmation.blade.php
+++ resources/views/admin/apartments/payment-confirmation.blade.php
@@ -1,10 +1,10 @@
 @extends('layouts.app')
-
-
 @section('content')
     <div class="container mt-5 d-flex justify-content-center align-items-center flex-column gap-3">
+
         <h1 class="text-center">Conferma del pagamento</h1>
         <h5 class="text-center">Hai sponsorizzato {{ $apartment->title }} per {{ $sponsorship->duration }} ore</h5>
         <a href="{{ route('admin.apartments.index') }}" class="btn btn-primary">Torna alla lista degli appartamenti</a>
+        
     </div>
 @endsection
