f39b9d8636b5954cecc561a76eb72fe22642859c
AlessiaOsman <alessia.osman@gmail.com>
2024-05-08 18:02:04 +0200

    fix

--- resources/views/admin/apartments/payment-confirmation.blade.php
+++ resources/views/admin/apartments/payment-confirmation.blade.php
@@ -1,5 +1,6 @@
 @extends('layouts.app')
 
+
 @section('content')
     <div class="container mt-5 d-flex justify-content-center align-items-center flex-column gap-3">
         <h1 class="text-center">Conferma del pagamento</h1>
