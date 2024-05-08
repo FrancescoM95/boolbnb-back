8e2490ef828404cdc0a40a077a834ffb27ff637a
leonardoolia <leonardooliaa@gmail.com>
2024-05-08 20:44:51 +0200

    fixed registration form

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -105,7 +105,7 @@
                                 {{-- ? CONFERMA PASSWORD --}}
                                 <div class="mb-4 row">
                                     <label for="password-confirm"
-                                        class="col-md-4 col-form-label text-md-right text-dark">{{ __('Conferma Password') }}</label>
+                                        class="col-md-4 col-form-label text-md-right text-dark">{{ __('Conferma Password *') }}</label>
 
                                     <div class="col-md-6">
                                         <input id="password-confirm" type="password" class="form-control"
