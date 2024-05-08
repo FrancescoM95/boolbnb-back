bd97646221ea54876b5da9b5b54d00581ba7c231
FrancescoM95 <mon95fran@gmail.com>
2024-05-08 13:07:15 +0200

    style change

--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -1,24 +1,20 @@
 @extends('layouts.app')
 @section('content')
     <div class="jumbotron">
-        <div class="container py-5">
-            <div class="home-bg p-3">
-                <h1 class="text-center"><img src="{{ Vite::asset('resources/img/Boolbnb-logo-nobg.png') }}" alt="Logo">
+        <div class="container h-100 py-3">
+            <div class="home-bg h-100">
+
+
+                <h1 class="text-center"><img src="{{ Vite::asset('resources/img/boolbnb-logo-nobg.png') }}" alt="Logo">
                 </h1>
 
-                <p class="fw-bold text-center mb-5">
-                    Che tu sia un viaggiatore in cerca di una sistemazione confortevole o un ospite desideroso di
-                    condividere la
-                    tua casa con il mondo, BoolbnB è qui per te.
-                </p>
 
-                <div class="d-flex flex-column justify-content-center align-items-center gap-3">
+                <div class="d-flex justify-content-center align-items-center gap-3 mt-4">
                     <a class="btn-donate" href="{{ route('login') }}">
-                        Log-in
+                        Accedi
                     </a>
 
                     <div class="d-flex flex-column justify-content-center align-items-center">
-                        <p class="fs-6">Non sei ancora registrato?</p>
 
                         @if (Route::has('register'))
                             <a class="btn-donate" href="{{ route('register') }}">Crea nuovo account</a>
